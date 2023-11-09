ff739f023c013123b46a8e614e0d49225aab7296
Jan Wedekind (Dr) <jan@wedesoft.de>
2023-11-09 20:46:15 +0000

    Test reading lines from file

--- src/mealmaster_distinct/core.clj
+++ src/mealmaster_distinct/core.clj
@@ -4,3 +4,7 @@
   "Get file names of input directory"
   (let [directory (or directory "input")]
     (map #(str directory \/ %) (.list (clojure.java.io/file directory)))))
+
+(defn lines [filename]
+  "Read lines from a text file"
+  (line-seq (clojure.java.io/reader filename)))
--- test/mealmaster_distinct/t_core.clj
+++ test/mealmaster_distinct/t_core.clj
@@ -5,3 +5,7 @@
 (fact "Get file names from input directory"
       (file-names "test/mealmaster_distinct/fixtures/file-names")
       => ["test/mealmaster_distinct/fixtures/file-names/testfile.txt"])
+
+(fact "Read lines from a unix file"
+      (lines "test/mealmaster_distinct/fixtures/lines/unix.txt")
+      => ["first line" "second line"])
