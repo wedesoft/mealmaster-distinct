1e6e8e02f3ae1c72007c990383b1985bcb766e5d
Jan Wedekind (Dr) <jan@wedesoft.de>
2023-11-09 21:22:56 +0000

    Check for Mealmaster header or footer.

--- src/mealmaster_distinct/core.clj
+++ src/mealmaster_distinct/core.clj
@@ -8,3 +8,15 @@
 (defn lines [file]
   "Read lines from a text file"
   (line-seq file))
+
+(defn filter-printable [line]
+  "Remove non-printable characters"
+  (apply str (re-seq #"[\r\n\t -\xFF]" line)))
+
+(defn footer? [line]
+  "Check whether line is a Mealmaster footer"
+  (#{"MMMMM" "-----"} line))
+
+(defn header? [line]
+  "Check whether line is a Mealmaster header"
+  (re-matches #"(-----|MMMMM)[^\r\n]*[Mm][Ee][Aa][Ll]-[Mm][Aa][Ss][Tt][Ee][Rr][^\r\n]*" line))
--- test/mealmaster_distinct/t_core.clj
+++ test/mealmaster_distinct/t_core.clj
@@ -13,3 +13,21 @@
 (fact "Read lines from a DOS file"
       (lines (clojure.java.io/reader (char-array "first line\r\nsecond line")))
       => ["first line" "second line"])
+
+(fact "Read past zeroes"
+      (lines (clojure.java.io/reader (char-array "text\000more text")))
+      => ["text\000more text"])
+
+(fact "Filter for printable characters"
+      (filter-printable "text\r\nmore\ttext \007\377 finish") => "text\r\nmore\ttext \377 finish")
+
+(facts "Check for footer"
+       (footer? "abc") => falsey
+       (footer? "MMMMM") => truthy
+       (footer? "-----") => truthy)
+
+(facts "Check for header"
+       (header? "abc") => falsey
+       (header? "MMMMM----- Meal-Master-Tools fuer Windows V0.1") => truthy
+       (header? "abc MMMMM----- Meal-Master-Tools fuer Windows V0.1") => falsey
+       (header? "---------- Meal-Master-Tools fuer Windows V0.1") => truthy)
